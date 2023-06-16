8922583942204f6d0a2594d8b9ccab2e557d9c2c
Aleksandr Ivanov <1300087@gmail.com>
2023-06-16 19:53:26 +0300

    add navbar, routerConfig

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,7 @@
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-import { Suspense } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { AppRouter } from 'app/providers/router';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { Navbar } from 'widgets/Navbar';
 import './styles/index.scss';
 
 const App = () => {
@@ -11,15 +9,9 @@ const App = () => {
 
   return (
     <div className={classNames('app', {}, [theme])}>
+      <Navbar />
+      <AppRouter />
       <button onClick={toggleTheme}>toggle</button>
-      <Link to={'/'}>MAIN</Link>
-      <Link to={'/about'}>ABOUT</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/' element={<MainPage />} />
-          <Route path='/about' element={<AboutPage />} />
-        </Routes>
-      </Suspense>
     </div>
   );
 };
