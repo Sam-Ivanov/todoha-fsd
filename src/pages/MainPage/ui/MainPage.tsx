054b63067dc693957ed826ed4571a0bec990861a
Aleksandr Ivanov <1300087@gmail.com>
2023-06-30 19:38:38 +0300

    add ci

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,16 +1,22 @@
 import { BugButton } from 'app/providers/ErrorBoundary';
 import { useTranslation } from 'react-i18next';
+import LogoIcon from 'shared/assets/icons/logobable.svg';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './MainPage.module.scss';
 
 const MainPage = () => {
   const { t } = useTranslation('main');
 
   return (
-    <div>
+    <>
       <BugButton />
       <div>
         {t('Главная страница')}
       </div>
-    </div>
+      <div className={cls.wrapper}>
+        <LogoIcon className={classNames(cls.logo)} />
+      </div>
+    </>
   );
 };
 
--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
+
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
