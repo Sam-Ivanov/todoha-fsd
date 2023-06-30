57ffba3420755cae031da4677b8ca5c17d246d38
Aleksandr Ivanov <1300087@gmail.com>
2023-06-30 19:48:24 +0300

    check ci

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,11 +1,5 @@
 type Mods = Record<string, boolean | string | undefined>;
 
 export function classNames(cls: string, mods: Mods = {}, additional: Array<string | undefined> = []): string {
-  return [
-    cls,
-    ...additional.filter(Boolean),
-    ...Object.entries(mods)
-      .filter((el) => Boolean(el[1]))
-      .map((el) => el[0]),
-  ].join(' ');
+  return 'qwe';
 }
